408f479b7ad1da3f5b474168db7057dbbbe65e6a
WilliamsRv <willams.rosas.v@vallegrande.edu.pe>
2024-04-01 07:12:02 -0700

    probando

---
+++ 12_WilliamsRosas/src/S05_14/persona.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S05_14;
+
+/**
+ *
+ * @author Lab10
+ */
+public class persona {
+    int edad; 
+    int dni;
+    String nombre;
+    String apellido;
+    String puesto;
+    
+    
+    public void correr(){
+        System.out.println("esta persona puede correr");
+    
+    }
+    
+    public void caminar (){
+    
+        System.out.println("puede caminar");
+    }
+    
+    public void saltar(){
+        System.out.println("puede saltat");
+    }
+}
